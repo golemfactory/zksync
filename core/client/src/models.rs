1bea7fd9d164cac7c4d916bb61f687522dffcc75
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-08-19 17:02:56 +0200

    Removed un-commented code

--- core/client/src/models.rs
+++ core/client/src/models.rs
@@ -18,30 +18,6 @@ pub struct ResponseAccountState {
     pub pub_key_hash: PubKeyHash,
 }
 
-// impl ResponseAccountState {
-//     pub fn try_restore(account: Account, tokens: &TokenDBCache) -> Result<Self> {
-//         let mut balances = HashMap::new();
-//         for (token_id, balance) in account.get_nonzero_balances() {
-//             if token_id == 0 {
-//                 balances.insert("ETH".to_string(), balance);
-//             } else {
-//                 let token = tokens
-//                     .get_token(token_id)
-//                     .ok()
-//                     .flatten()
-//                     .ok_or_else(Error::internal_error)?;
-//                 balances.insert(token.symbol.clone(), balance);
-//             }
-//         }
-//
-//         Ok(Self {
-//             balances,
-//             nonce: account.nonce,
-//             pub_key_hash: account.pub_key_hash,
-//         })
-//     }
-// }
-
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct DepositingFunds {
@@ -55,44 +31,6 @@ pub struct DepositingFunds {
 pub struct DepositingAccountBalances {
     balances: HashMap<String, DepositingFunds>,
 }
-
-// impl DepositingAccountBalances {
-//     pub fn from_pending_ops(
-//         pending_ops: OngoingDepositsResp,
-//         tokens: &TokenDBCache,
-//     ) -> Result<Self> {
-//         let mut balances = HashMap::new();
-//
-//         for op in pending_ops.deposits {
-//             let token_symbol = if op.token_id == 0 {
-//                 "ETH".to_string()
-//             } else {
-//                 tokens
-//                     .get_token(op.token_id)
-//                     .map_err(|_| Error::internal_error())?
-//                     .ok_or_else(Error::internal_error)?
-//                     .symbol
-//             };
-//
-//             let expected_accept_block =
-//                 op.received_on_block + pending_ops.confirmations_for_eth_event;
-//
-//             let balance = balances
-//                 .entry(token_symbol)
-//                 .or_insert_with(DepositingFunds::default);
-//
-//             balance.amount += BigUint::from(op.amount);
-//
-//             // `balance.expected_accept_block` should be the greatest block number among
-//             // all the deposits for a certain token.
-//             if expected_accept_block > balance.expected_accept_block {
-//                 balance.expected_accept_block = expected_accept_block;
-//             }
-//         }
-//
-//         Ok(Self { balances })
-//     }
-// }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -146,32 +84,6 @@ pub struct OngoingDeposit {
     eth_tx_hash: String,
 }
 
-// impl OngoingDeposit {
-//     pub fn new(received_on_block: u64, priority_op: PriorityOp) -> Self {
-//         let (token_id, amount) = match priority_op.data {
-//             FranklinPriorityOp::Deposit(deposit) => (
-//                 deposit.token,
-//                 deposit
-//                     .amount
-//                     .to_u128()
-//                     .expect("Deposit amount should be less then u128::max()"),
-//             ),
-//             other => {
-//                 panic!("Incorrect input for OngoingDeposit: {:?}", other);
-//             }
-//         };
-//
-//         let eth_tx_hash = hex::encode(&priority_op.eth_hash);
-//
-//         Self {
-//             received_on_block,
-//             token_id,
-//             amount,
-//             eth_tx_hash,
-//         }
-//     }
-// }
-
 /// Information about ongoing deposits for certain recipient address.
 ///
 /// Please note that since this response is based on the events that are
@@ -213,25 +125,3 @@ pub enum RpcErrorCodes {
     AccountCloseDisabled = 301,
     OperationsLimitReached = 302,
 }
-
-// impl From<TxAddError> for RpcErrorCodes {
-//     fn from(error: TxAddError) -> Self {
-//         match error {
-//             TxAddError::NonceMismatch => Self::NonceMismatch,
-//             TxAddError::IncorrectTx => Self::IncorrectTx,
-//             TxAddError::TxFeeTooLow => Self::FeeTooLow,
-//             TxAddError::MissingEthSignature => Self::MissingEthSignature,
-//             TxAddError::EIP1271SignatureVerificationFail => Self::EIP1271SignatureVerificationFail,
-//             TxAddError::IncorrectEthSignature => Self::IncorrectEthSignature,
-//             TxAddError::ChangePkNotAuthorized => Self::ChangePkNotAuthorized,
-//             TxAddError::Other => Self::Other,
-//             TxAddError::DbError => Self::Other,
-//         }
-//     }
-// }
-
-// impl Into<ErrorCode> for RpcErrorCodes {
-//     fn into(self) -> ErrorCode {
-//         (self as i64).into()
-//     }
-// }
